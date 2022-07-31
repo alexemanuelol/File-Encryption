aee7673a244064051569cba73714c173889dc10c
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2022-07-31 11:23:12 +0200

    Made the script fully functional

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.7.3'
+ruby '3.1.2'
 
 gem 'openssl', '~> 2.2'
 gem 'base64', '~> 0.1.0'
--- src/crypto.rb
+++ src/crypto.rb
@@ -9,6 +9,60 @@ require 'base64'
 require 'optparse'
 
 
+class Cryptography
+    # Cryptography class
+
+    def initialize()
+    end
+
+    def encrypt_data(data, password)
+        cipher = OpenSSL::Cipher::Cipher.new('AES-256-CBC')
+        cipher.encrypt
+        cipher.key = OpenSSL::Digest::SHA256.new(password).digest
+        return Base64.encode64(cipher.update(data.to_s.strip) + cipher.final)
+    end
+
+    def encrypt_file(path, password)
+        dirname = File.dirname(path)
+        filename = File.basename(path, '.*')
+        extension = File.extname(path)
+        output = dirname + '/' + filename + '_encrypted' + extension
+
+        buf = ''
+        File.open(output, 'wb') do |outf|
+            File.open(path, 'rb') do |inf|
+                data = inf.read
+                encrypted = self.encrypt_data(data, password)
+                outf.write(encrypted)
+            end
+        end
+    end
+
+    def decrypt_data(data, password)
+        base64_decoded = Base64.decode64(data.to_s.strip)
+        cipher = OpenSSL::Cipher::Cipher.new('AES-256-CBC')
+        cipher.decrypt
+        cipher.key = OpenSSL::Digest::SHA256.new(password).digest
+        return cipher.update(base64_decoded) + cipher.final
+    end
+
+    def decrypt_file(path, password)
+        dirname = File.dirname(path)
+        filename = File.basename(path, '.*')
+        extension = File.extname(path)
+        output = dirname + '/' + filename + '_decrypted' + extension
+
+        File.open(output, 'wb') do |outf|
+            File.open(path, 'rb') do |inf|
+                data = inf.read
+                decrypted = self.decrypt_data(data, password)
+                outf.write(decrypted)
+            end
+        end
+    end
+end
+
+
 class ArgParser
     # Handles incoming CLI arguments
 
@@ -35,6 +89,10 @@ class ArgParser
                 options[:decrypt] = decrypt
             end
 
+            opts.on("-p", "--password PASSWORD", "The password for encrypt/decrypt.") do |password|
+                options[:password] = password
+            end
+
             opts.on("-h", "--help", "Display this help text and exit.") do
                 puts opts
                 exit
@@ -53,10 +111,39 @@ class ArgParser
             exit 1
         end
 
+        if !options.key?(:password)
+            puts 'Password is required.'
+            puts opts
+            exit 1
+        end
+
+        if !options.key?(:encrypt) && !options.key?(:decrypt)
+            puts 'Choose file to encrypt/decrypt.'
+            puts opts
+            exit 1
+        end
+
         return options
     end
 end
 
 
 options = ArgParser.parse(ARGV)
-puts options
+crypt = Cryptography.new()
+
+begin
+    if options.key?(:encrypt)
+        crypt.encrypt_file(options[:encrypt], options[:password])
+        puts 'Successfully encrypted.'
+    elsif options.key?(:decrypt)
+        crypt.decrypt_file(options[:decrypt], options[:password])
+        puts 'Successfully decrypted.'
+    end
+rescue Exception => e
+    if options.key?(:encrypt)
+        puts "Could not successfully encrypt file: #{options[:encrypt]}"
+    elsif options.key?(:decrypt)
+        puts "Could not successfully decrypt file: #{options[:decrypt]}"
+        puts 'Password might be wrong.'
+    end
+end
